a20c0a8bb20521bae6c440adddc999f8e76657e2
dirtyb1rd <yeskov777.888@gmail.com>
2023-12-20 19:47:43 +0300

    first commit

---
+++ src/main/java/ru/gb/javacore/Main.java
@@ -0,0 +1,7 @@
+package ru.gb.javacore;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Note.addNote("Hello World!"));
+    }
+}
---
+++ src/main/java/ru/gb/javacore/Note.java
@@ -0,0 +1,8 @@
+package ru.gb.javacore;
+
+public class Note {
+
+    public static String addNote(String note){
+        return note;
+    }
+}
